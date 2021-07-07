614c166449a9ce1f6a34dd50fe36f0e64a10146a
mihon.testosterone <mihon.testosterone@gmail.com>
2021-07-07 19:39:11 +0300

    Revert "1"
    This reverts commit 80724655c149bc32e3cb39c162f1bc9120519107.

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 function slidesPlugin() {
 
     const slides = document.querySelectorAll(".slide")
+    console.log(slides.length);
     let activeSlide = getRandomInt(slides.length-1)
+    console.log(activeSlide);
     slides[activeSlide].classList.add('active')
     for (const slide of slides) {
     slide.addEventListener("click", () => {
