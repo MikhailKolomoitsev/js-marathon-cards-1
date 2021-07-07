80724655c149bc32e3cb39c162f1bc9120519107
mihon.testosterone <mihon.testosterone@gmail.com>
2021-07-07 19:37:57 +0300

    1
    1

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 function slidesPlugin() {
 
     const slides = document.querySelectorAll(".slide")
-    console.log(slides.length);
     let activeSlide = getRandomInt(slides.length-1)
-    console.log(activeSlide);
     slides[activeSlide].classList.add('active')
     for (const slide of slides) {
     slide.addEventListener("click", () => {
